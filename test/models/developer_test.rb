3919dc54fec2f7f7efbc71c6ffa87519aea4efe0
Frank Meza <frankmezapdx@gmail.com>
2017-05-27 14:09:55 -0700

    refactor tests

--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -1,23 +1,30 @@
 require 'test_helper'
 
 class DeveloperTest < ActiveSupport::TestCase
+  setup do
+    @frank = Developer.new(username: 'frank', email: 'fr@nk.io', password: 'mashape')
+  end
+
+  teardown do
+    @frank.destroy
+  end
+
   test 'a valid Developer has a username, email address, and password' do
-    frank = Developer.new(username: 'frank', email: 'frank@computerblue.com', password: 'california2017')
-    assert frank.valid?
+    assert @frank.valid?
   end
 
   test 'a Developer requires a username' do
-    frank = Developer.new(email: 'frank@computerblue.com', password: 'california2017')
-    assert_not frank.valid?
+    @frank.username = nil
+    assert_not @frank.valid?
   end
 
   test 'a Developer requires an email address' do
-    frank = Developer.new(username: 'frank', password: 'california2017')
-    assert_not frank.valid?
+    @frank.email = nil
+    assert_not @frank.valid?
   end
 
   test 'a Developer requires a password' do
-    frank = Developer.new(username: 'frank', email: 'frank@computerblue.com')
-    assert_not frank.valid?
+    @frank.password = nil
+    assert_not @frank.valid?
   end
 end
