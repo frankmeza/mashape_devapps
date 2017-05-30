b8a16102a930559e7c7fd1f4e50a70f9c37ab7de
Frank Meza <frankmezapdx@gmail.com>
2017-05-29 21:24:55 -0700

    slight change for more readable failures

--- test/controllers/developers_controller_test.rb
+++ test/controllers/developers_controller_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class DevelopersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @new_dev = Developer.new()
     @frank = Developer.create(username: 'frank', email: 'fr@nk.io', password: 'mashape')
     @meza = Developer.create(username: 'meza', email: 'm@za.io', password: 'mashape')
     @admin = Admin.create!(email: 'testadmin@yourapp.com', password: 'cloak_and_dagger')
@@ -22,7 +21,7 @@ class DevelopersControllerTest < ActionDispatch::IntegrationTest
 
   test 'INDEX - GET /developers' do
     get '/developers', headers: @admin_headers
-    assert_equal status, 200
+    assert_equal 200, status
     json = JSON.parse body
     json['developers'].include? @frank.as_json
     json['developers'].include? @meza.as_json
@@ -30,21 +29,22 @@ class DevelopersControllerTest < ActionDispatch::IntegrationTest
 
   test 'SHOW - GET /developers/:id' do
     get "/developers/#{@frank.id}", headers: @admin_headers
-    assert_equal status, 200
+    assert_equal 200, status
     json = JSON.parse body
     assert_equal json['developer'], @frank.as_json
   end
 
   test 'NEW - GET /developers/new' do
+    new_dev = Developer.new()
     get '/developers/new', headers: @admin_headers
-    assert_equal status, 200
+    assert_equal 200, status
     json = JSON.parse body
-    assert_equal json['developer'], @new_dev.as_json
+    assert_equal json['developer'], new_dev.as_json
   end
 
   test 'EDIT - GET /developers/:id/edit' do
     get "/developers/#{@frank.id}/edit", headers: @admin_headers
-    assert_equal status, 200
+    assert_equal 200, status
     json = JSON.parse body
     assert_equal json['developer'], @frank.as_json
   end
@@ -52,13 +52,13 @@ class DevelopersControllerTest < ActionDispatch::IntegrationTest
   test 'CREATE SUCCESS - POST /developers' do
     valid_dev = { username: 'dev', email: 'dev@email.com', password: 'devdevdev' }
     post '/developers', params: { developer: valid_dev }, headers: @admin_headers
-    assert_equal status, 201
+    assert_equal 201, status
   end
 
   test 'CREATE ERROR - POST /developers' do
     dev_without_email = { username: 'dev', password: 'devdevdev' }
     post '/developers', params: { developer: dev_without_email }, headers: @admin_headers
-    assert_equal status, 422
+    assert_equal 422, status
     json = JSON.parse body
     json['errors']['email'].include? "can't be blank"
   end
@@ -66,20 +66,20 @@ class DevelopersControllerTest < ActionDispatch::IntegrationTest
   test 'UPDATE SUCCESS - PUT /developers/:id' do
     new_name = { username: 'Frankyboy' }
     put "/developers/#{@frank.id}", params: { developer: new_name }, headers: @admin_headers
-    assert_equal status, 204
+    assert_equal 204, status
   end
 
   test 'UPDATE ERROR - PUT /developers/:id' do
     no_name = { username: '' }
     put "/developers/#{@frank.id}", params: { developer: no_name }, headers: @admin_headers
-    assert_equal status, 422
+    assert_equal 422, status
     json = JSON.parse body
     json['errors']['username'].include? "can't be blank"
   end
 
   test 'DELETE - DELETE /developers/:id' do
     delete "/developers/#{@frank.id}", headers: @admin_headers
-    assert_equal status, 204
+    assert_equal 204, status
     assert_nil Developer.find_by(username: 'frank')
   end
 end
